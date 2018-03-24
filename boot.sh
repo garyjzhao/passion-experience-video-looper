fcbf7791ce040d4aa32572d6a0a34d24441fe512
jonyen <jonyen@gmail.com>
2018-03-23 17:21:35 -0700

    Update boot.sh
    Ctrl-C code to test interrupt (from https://www.raspberrypi.org/forums/viewtopic.php?t=10218&start=25)

--- boot.sh
+++ boot.sh
@@ -2,3 +2,18 @@
 
 sleep 10
 omxplayer --loop --aspect-mode=fill --no-osd /home/pi/pe/PE2016-Golgotha_Clouds_LOOP.mp4
+
+##!/bin/sh -x
+#trap exit 1 SIGINT SIGKILL SIGTERM
+
+#SERVICE='omxplayer'
+#while true;do
+#if ps ax | grep -v grep | grep $SERVICE > /dev/null
+#then echo "running" # sleep 1
+#else
+#xterm -fullscreen -fg black - bg black -e | omxplayer --loop --aspect-mode=fill --no-osd -o hdmi /home/pi/1.mpg
+
+#SIGINT
+
+#fi
+#done
